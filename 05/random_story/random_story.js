afc5b40418601b1f70e3bdcd754c6ba16ab167d2
slipkinem <398873901@qq.com>
2016-11-08 18:39:47 +0800

    Rejected commit: Default

---
+++ 05/random_story/random_story.js
@@ -0,0 +1,71 @@
+/**
+ * Created by slipkinem on 2016/11/8.
+ */
+
+// 串化 流程控制
+const fs = require('fs');
+const request = require('request');
+const htmlparder = require('htmlparser');
+const configFileName = './rss_feeds.txt';
+
+function checkForRSSFile() {
+    fs.exists(configFileName,function (exists) {
+        if (!exists){
+            return next(new Error('error' + configFileName));
+        }else {
+            next(null,configFileName);
+        }
+    })
+}
+
+function readRSSFile(configFileName) {
+    fs.readFile(configFileName,function (err, feedList) {
+        if (err) return next(err);
+
+        var feedList = feedList
+                            .toString()
+                            .replace(/^\s+|\s+$/g, '')
+                            .split('\n');
+        var random = Math.floor(Math.random()*feedList.length);
+        next(null,feedList[random]);
+    })
+}
+
+function downLoadRSSFeed(feedUrl) {
+    request({url:feedUrl},function (err, resquest, responseBody) {
+        if(err) next(err);
+        if (resquest.statusCode != 200){
+            return next(new Error('code is fail'));
+        }else {
+            next(null,responseBody);
+        }
+    });
+}
+
+function parserRSSFees(rss) {
+    var handler = new htmlparder.RssHandler();
+    var parser = new htmlparder.Parser(handler);
+    parser.parseComplete(rss);
+
+    if (!handler.dom.items.length){
+        return next (new Error('no rss item found'));
+    }
+
+    var item = handler.dom.items.shift();
+    console.log(item.title);
+    console.log(item.link);
+}
+
+var tasks = [checkForRSSFile,readRSSFile,downLoadRSSFeed,parserRSSFees];
+
+function next(err, result) {
+    if (err) throw err;
+
+    var currentTask = tasks.shift();
+
+    if (currentTask){
+        currentTask(result);
+    }
+}
+
+next();
